67104473fcf5b9a33bfebe540ef3a10436e36af6
Jeff Williams <jeff.williams@addrobots.com>
2017-12-28 20:57:01 -0800

    #1 fixes to the motor control dial (plus refactor)

--- dialcontrol-wip/app/dial.js
+++ dialcontrol-wip/app/dial.js
@@ -14,21 +14,21 @@ function Dial(startNum, stopNum, startAs, id) {
 	this.stN = startNum;
 	this.spN = stopNum;
 	this.current = startAs;
-	this.id = document.getElementById(id);
-	this.reset = function() {
+	this.dialElement = document.getElementById(id);
+	this.reset = function () {
 		this.current = startAs;
 	};
-	this.getCurrent = function() {
+	this.getCurrent = function () {
 		return this.current;
 	};
-	this.render = function() {
-		var tempstart = this.stN;
-		var tempstop = this.spN;
-		var tempcurrent = this.getCurrent();
+	this.render = function () {
+		let tempstart = this.stN;
+		let tempstop = this.spN;
+		let tempcurrent = this.getCurrent();
 
-		this.id.innerHTML = "<div class='dialjs'><div class='dialjs-inner' style='transform: rotate( " + this.getCurrent() + "deg ) translateY( 30px ) rotate( -" + this.getCurrent() +"deg );'></div></div>";
+		this.dialElement.innerHTML = "<div class='dialjs'><div class='dialjs-inner' style='transform: rotate( " + this.getCurrent() + "deg ) translateY( 30px ) rotate( -" + this.getCurrent() + "deg );'></div></div>";
 	};
-	this.update = function(newval) {
+	this.update = function (newval) {
 		if (newval >= this.stN && newval <= this.spN) {
 			this.current = newval;
 			this.render();
@@ -36,11 +36,11 @@ function Dial(startNum, stopNum, startAs, id) {
 			console.error("dial.js: error while updating (attempted to set value to " + newval + ", that's outside the range!");
 		}
 	};
-	this.overlap = function() {
-		if (this.current == this.stN) {
+	this.overlap = function () {
+		if (this.current === this.stN) {
 			this.current = this.spN;
 			this.render();
-		} else if (this.current == this.spN) {
+		} else if (this.current === this.spN) {
 			this.current = this.stN;
 			this.render();
 		}
@@ -48,24 +48,22 @@ function Dial(startNum, stopNum, startAs, id) {
 }
 
 function DialControls(dial) {
-	var mousedown, mCrds;
-	this.id = null;
+	let mousedown = false;
+	this.dialElement = null;
 	if (dial === undefined) {
 		console.error("dial.js: No dial for dial controls");
 		delete this;
 	} else {
-		this.id = dial.id;
+		this.dialElement = dial.dialElement;
 	}
-	this.rectCoords = this.id.getBoundingClientRect();
-	var coords = {
-		x: parseInt(40, 10),
-		y: parseInt(40, 10)
-	}
+	let dialRect = this.dialElement.getBoundingClientRect();
+	let centerX = dialRect.left + (80 / 2);
+	let centerY = dialRect.top + (dialRect.height / 2);
 	this.click = false;
 	this.lock = function () {
-		if (this.islocking == true) {
+		if (this.islocking === true) {
 			this.islocking = false;
-		} else if (this.islocking == false) {
+		} else if (this.islocking === false) {
 			this.islocking = true;
 		}
 		islock = this.islocking;
@@ -73,21 +71,18 @@ function DialControls(dial) {
 	this.removeControls = function () {
 		console.log(delete this);
 	};
-	this.id.onmousedown = function () {
+	this.dialElement.onmousedown = function () {
 		mousedown = true;
 	};
 	document.body.onmouseup = function () {
 		mousedown = false;
 	};
-	document.body.onmousemove = function (e) {
-		if (mousedown == true) {
-			var angle = 0;
-			mCrds = {
-				x: e.pageX,
-				y: e.pageY
-			};
-			var foo = Math.atan2(coords.y - mCrds.y, coords.x - mCrds.x);
-			angle = foo * (180 / Math.PI);
+	document.body.onmousemove = function (event) {
+		if (mousedown === true) {
+			let angle = Math.atan2(centerY - event.pageY, centerX - event.pageX) * 180 / Math.PI;
+			if (angle < 0) {
+				angle += 360;
+			}
 			console.log();
 			dial.update(angle);
 		}
