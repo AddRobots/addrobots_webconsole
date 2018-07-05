c73e5b336c7ed7b27897632ab3ea738610b35fd3
Jeff Williams <jeff.williams@addrobots.com>
2018-07-05 03:46:18 -0700

    #32 fix support for computing a jwt token in the cloud function

--- firebase-cloud-functions/functions/index.js
+++ firebase-cloud-functions/functions/index.js
@@ -1,9 +1,25 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
 const admin = require('firebase-admin');
-const google = require('googleapis');
+const {google} = require('googleapis');
 
 admin.initializeApp(functions.config().firebase);
+
+/*
+	Firebase Cloud Messaging doesn't allow remotes to send messages securely!
+
+	This cloud function is a bridge that makes that possible securely.
+
+	We store the Firebase Cloud Message API private key in the Firebase database
+	so that we don'thave to include it in the stored code. Then this cloud function
+	gets called by a remote (e.g. Android, Javascript) using the logged-in user's
+	current uid-derived temp token. This token is used to gain secure access
+	to the Firebase database so we can get the private API key. We then use
+	that private key to make a JWT temp-token call to the server.
+
+	If we succeed, we pass that token back to the caller, so that they can
+	use it to send Firebase Cloud Messages.
+ */
 
 exports.getOAuthToken = functions.https.onRequest((req, res) => {
 	cors(req, res, () => {
@@ -20,12 +36,16 @@ exports.getOAuthToken = functions.https.onRequest((req, res) => {
 							getAccessToken(privateKey, email)
 								.then(authToken => {
 									res.status(200).send(authToken);
-								});
+								})
+								.catch((err) => res.status(401).send(err.message));
+						} else {
+							res.status(401).send('bad snapshot')
 						}
 					})
-					.catch((err) => res.status(401).send(err));
+					.catch((err) => res.status(401).send(err.message));
+
 			})
-			.catch((err) => res.status(401).send(err));
+			.catch((err) => res.status(401).send(err.message));
 	});
 });
 
